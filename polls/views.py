d2ae2b8f0d36d9c2c0336394c26bcd631e01549e
gingerheef003 <gingerheef003@gmail.com>
2020-12-23 20:32:46 +0530

    add vote and results

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Question
+from django.urls import reverse
+
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -19,7 +22,22 @@ def detail(request, question_id):
     })
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question
+    })
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
